cc8506ab7ac2db853510d425b8e323ea8ee49870
Kevin Botero <kbotero201@gmail.com>
2020-09-07 16:59:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,9 +4,9 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push(next_color)
 end
 
-def using_push(array, string)
-  array.push(string)
-end
+#def using_push(array, string)
+#  array.push(string)
+#end
 
 def using_unshift(array, string)
 array.unshift(string)
