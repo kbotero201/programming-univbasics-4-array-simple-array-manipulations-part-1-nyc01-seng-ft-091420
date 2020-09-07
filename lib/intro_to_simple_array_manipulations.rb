b6c881cf10560df435de5126da9db23a5e445233
Kevin Botero <kbotero201@gmail.com>
2020-09-07 16:52:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,3 +10,8 @@ def using_pop(great_hits_of_the_nineties)
   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
   great_hits_of_the_nineties.pop
 end
+
+def pop_with_args(chars_in_game_of_thrones)
+  chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
+  chars_in_game_of_thrones.pop(2)
+end
